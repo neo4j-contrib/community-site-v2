6b8b83e92145b573cf9161aa6a515252b758d623
Robert Barrow <merefield@gmail.com>
2020-03-13 23:34:06 +0000

    FIX: now filter out pinned topics

--- lib/select_topics.rb
+++ lib/select_topics.rb
@@ -4,7 +4,7 @@ class DiscourseNeo4j::ListTopics
     Topic
       .select("topics.id,topics.title,topics.excerpt,topics.user_id,topics.posts_count,topics.views,topics.like_count,users.username,users.uploaded_avatar_id")
       .joins("LEFT OUTER JOIN users ON users.id = topics.user_id")    
-      .where(category_id: category_id)
+      .where(category_id: category_id, pinned_at: nil)
       .take(quantity)
   end
 
